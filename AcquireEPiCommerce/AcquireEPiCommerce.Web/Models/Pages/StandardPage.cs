0818300572a2a0fd5c39b0e34fbf92a0736c8ad0
Dmitrijs Blinkovs <dmitry.blinkov@outlook.com>
2018-09-17 15:34:16 +0300

    1. Added page type "Standard Page" with simple main text, heading and content area. 2. Addd support for media files, needed to have opportunity to upload media files. 2. Added commerce project.

---
+++ AcquireEPiCommerce/AcquireEPiCommerce.Web/Models/Pages/StandardPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AcquireEPiCommerce.Web.Models.Pages
+{
+    [ContentType(
+        DisplayName = "Standard Page", 
+        GUID = "ec7090c1-8aa2-40b6-ae70-56df762ac0c4", 
+        Description = "")]
+    [ImageUrl("~/Content/Icons/IconStandardPage.png")]
+    public class StandardPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "The heading of the page, there you can insert text of heading, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 0)]
+        public virtual String Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Content area",
+            Description = "The content area of the page, using to place inside the blocks of page.",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        public virtual ContentArea ContentArea { get; set; }
+
+
+    }
+}
---
+++ AcquireEPiCommerce/AcquireEPiCommerce.Web/Models/Pages/StartPage.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AcquireEPiCommerce.Web.Models.Pages
+{
+    [ContentType(
+        DisplayName = "Start Page", 
+        GUID = "8b66f09a-7035-469b-9738-d3067e67db76", 
+        Description = "The main root page pattern.")]
+    [ImageUrl("~/Content/Icons/IconStartPage.png")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The page is used as the main container under which will be placed all other pages.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
